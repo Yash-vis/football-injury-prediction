62048717aa2134b856433cf9185b87ab0551cfa3
Yash Vishwakarma <yash20vi@gmail.com>
2025-08-20 09:03:41 +0530

    Removed unnecessary comments

--- main.py
+++ main.py
@@ -8,40 +8,28 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import joblib as jl
-# Load data
-data = pd.read_csv('football/foot.csv')
 
-# Encode categorical column
+
+data = pd.read_csv('football/foot.csv')
 encode = LabelEncoder()
 data['Position'] = encode.fit_transform(data['Position'])
 
-# Columns to round and scale
 columns_round = [
     'Training_Hours_Per_Week', 'Knee_Strength_Score', 'Hamstring_Flexibility',
     'Reaction_Time_ms', 'Balance_Test_Score', 'Sprint_Speed_10m_s', 'Agility_Score',
     'Sleep_Hours_Per_Night', 'Stress_Level_Score', 'Nutrition_Quality_Score', 'BMI'
 ]
 
-# Round numeric columns
 data[columns_round] = data[columns_round].round(2)
 
-# Split features and target
+
 drop_col = ['Injury_Next_Season','Knee_Strength_Score', 'Hamstring_Flexibility',
     'Reaction_Time_ms', 'Balance_Test_Score', 'Sprint_Speed_10m_s', 'Agility_Score','Nutrition_Quality_Score']
 X = data.drop(drop_col, axis=1)
 y = data['Injury_Next_Season']
 
-# Train/test split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.25, random_state=42)
 
-# Scale numeric columns
-# scaler = StandardScaler()
-# X_train[columns_round] = scaler.fit_transform(X_train[columns_round])
-# X_test[columns_round] = scaler.transform(X_test[columns_round])
-
-# List of models to evaluate
 models = {
     "Logistic Regression": LogisticRegression(max_iter=1000),
     "Random Forest": RandomForestClassifier(random_state=42),
@@ -60,7 +48,7 @@ for name, model in models.items():
     print(f"  Test Accuracy: {test_acc:.4f}")
     print("-" * 40)
 model_path = jl.dump(models['Logistic Regression'], 'injury.pkl')
-print("✅ Model saved to:", model_path[0])
+print("Model saved to:", model_path[0])
 
 columns_path = jl.dump(X.columns.to_list(), 'columns.pkl')
-print("✅ Columns saved to:", columns_path[0])
+print("Columns saved to:", columns_path[0])
